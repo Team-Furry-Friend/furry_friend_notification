0d5aa4376224ed4c52e5d88d947636b6b56e1352
youngsoosoo <lovepys01@naver.com>
2023-08-12 15:42:10 +0900

    알림 서비스 기본 설정

---
+++ src/main/java/v3/furry_friend_notification/FurryFriendNotificationApplication.java
@@ -0,0 +1,13 @@
+package v3.furry_friend_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurryFriendNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FurryFriendNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/v3/furry_friend_notification/FurryFriendNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package v3.furry_friend_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurryFriendNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
